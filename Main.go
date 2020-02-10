cb305b287876aab30bf804280917594c650b865f
amikar <adivij@student.fitchburgstate.edu>
2020-02-09 20:26:49 -0800

    basic implementation with redis

--- Main.go
+++ Main.go
@@ -4,15 +4,69 @@ import (
 	"fmt"
 	"net/http"
 	"github.com/gorilla/mux"
-
+	"github.com/go-redis/redis/v7"
+	// "log"
+	"path"
+	"time"
+	
+	"encoding/json"
 )
+
+type SiteInfo struct {
+	LongUrl  string  `redis:"longurl" json:"longurl"`
+	Count  int  `redis:"count" json:"count"`
+
+}
+
+var rdb *redis.Client
+
+func init() {
+	rdb = redis.NewClient(&redis.Options{
+		Addr:         ":6379",
+		DialTimeout:  10 * time.Second,
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 30 * time.Second,
+		PoolSize:     10,
+		PoolTimeout:  30 * time.Second,
+	})
+}
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/hello", handler).Methods("GET")
+	r.HandleFunc("/getstats/{id}", handler).Methods("GET")
 	http.ListenAndServe(":8080", r)
+
 }
 
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World!")
+
+	urlSuffix := path.Base(r.URL.String())
+	fmt.Println(urlSuffix)
+
+	s, _ := rdb.Get(urlSuffix).Result()
+	if (s != ""){
+		var resInfo SiteInfo
+		json.Unmarshal([]byte(s), &resInfo)
+		countres := resInfo.Count + 1
+		json, err := json.Marshal(SiteInfo{LongUrl: resInfo.LongUrl, Count: countres})
+		if err != nil {
+			fmt.Println(err)
+		}
+		err = rdb.Set(urlSuffix, json, 0).Err()
+		if err != nil {
+			fmt.Println(err)
+		}
+	} else {
+		json, err := json.Marshal(SiteInfo{LongUrl: "Elliot", Count: 1})
+		if err != nil {
+			fmt.Println(err)
+		}
+		err = rdb.Set(urlSuffix, json, 0).Err()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	fmt.Fprintf(w, s)
 }
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/amikar/urlShortner
+
+go 1.13
+
+require (
+	github.com/go-redis/redis/v7 v7.0.0-beta.6 // indirect
+	github.com/gorilla/mux v1.7.3
+)
